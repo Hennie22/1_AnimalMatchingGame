761d4a26a2704740aba0471a9e0d5de3037e0bd2
Gebruiker <Gebruiker@DESKTOP-7GOVUAB>
2022-09-12 11:06:24 +0200

    Add project files.

---
+++ 1_AnimalMatchingGame/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1_AnimalMatchingGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+
+
+
+            // ❤✡🦢🦜🦚🐧🚗🚲😂😀😑😮🐵🐶🦁🐴🦓🐒🦏🐪🐘🐕🐍🐇🐟🐠🦜🦀🦋🐄🦛🦏 mbv raampje punt van Windows
+            // 🐻🦁🐘🐈🐟🐕‍🦺🐒🐧🦜🦢🦚❤💛💙✝🔴🟡🔷🔶 uit WhatsApp gekopieerd, kleuren edm verdwijnt
+            // 
+        }
+
+        private void SetUpGame()
+        {
+            //throw new NotImplementedException();
+
+            List<string> animalEmoji = new List<string>()
+            {
+                "🦏","🦏",
+                "🐪","🐪",
+                "🐘","🐘",
+                "🐕","🐕",
+                "🐍","🐍",
+                "🐇","🐇",
+                "🐟","🐟",
+                "🦋","🦋",
+            };
+            Random random = new Random();
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
