4a2f377a54dff19f58bf185bb4e549c9d19614fa
HJ van Dijk <vandijkha@ziggo.nl>
2022-09-15 15:14:25 +0200

    Update game

--- 1_AnimalMatchingGame/MainWindow.xaml.cs
+++ 1_AnimalMatchingGame/MainWindow.xaml.cs
@@ -56,5 +56,30 @@ namespace _1_AnimalMatchingGame
                 animalEmoji.RemoveAt(index);
             }
         }
+
+
+        TextBlock lastTextBlockClicked;
+        private bool findingMatch = false; //was:  bool findingMatch = false; 
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible; findingMatch = false;
+            }
+        }
+
     }
 }
